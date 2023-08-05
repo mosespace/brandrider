a4e7e9516afd46dabf2c5b16ea506b9a673e34ef
mosespace <kisakyemoses50@gmail.com>
2023-08-05 23:07:58 +0300

    feat: worked on the nav

---
+++ components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Bg from "../public/brandRider-BG2.jpg";
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <div className='hero-section'>
+      <div className='bg-image'>
+        <Image src={Bg} alt='BrandRider Image Here' fill />
+      </div>
+      <div className='text-overlay'>
+        <h1>Welcome to Our Website</h1>
+        <p>Discover Amazing Content</p>
+      </div>
+    </div>
+  );
+}
