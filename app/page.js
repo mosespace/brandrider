7807b3a12023cc7685d6f0d47fcaa292511b8cc1
mosespace <kisakyemoses50@gmail.com>
2023-08-07 18:23:57 +0300

    feat: added new componet

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import AboutUs from "@/components/AboutUs";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
@@ -11,7 +12,8 @@ export default function Home() {
   return (
     <>
       <Header />
-      <Hero/>
+      <Hero />
+      <AboutUs />
     </>
   );
 }
