f506eab8e8c971d3f8aaf360f5b52aa87d6d3011
mosespace <kisakyemoses50@gmail.com>
2023-11-23 11:23:53 +0300

    feta: initial commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
 import Launch from "@/components/Launch";
-import MyLogo from "../public/logo.svg";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className='flex min-h-screen items-center justify-center'>
-      {/* <Image src={MyLogo} /> */}
       <Launch />
     </main>
   );
