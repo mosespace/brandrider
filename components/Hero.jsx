80d6c848b49a86a168d02202391af89fabc1221c
mosespace <kisakyemoses50@gmail.com>
2023-12-18 11:31:56 +0300

    feat: initial commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -22,7 +22,7 @@ export default function Hero() {
             </div>
           </div>
         </div>
-        <div className='relative items-center w-full py-12 pb-12 mx-auto mt-12 max-w-7xl'>
+        <div className='relative items-center w-full py-12 pb-12 mx-auto mt-8 md:mt-12 max-w-7xl'>
           <svg
             fill='none'
             viewBox='0 0 400 400'
